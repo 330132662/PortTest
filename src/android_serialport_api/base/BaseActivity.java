db758dc9c0ba630541ac2cebb7dcbaffb42fd5c6
330132662@qq.com <330132662@qq.com>
2017-07-31 10:33:34 +0800

    上周改了没实测   这是实测后的

--- src/android_serialport_api/base/BaseActivity.java
+++ src/android_serialport_api/base/BaseActivity.java
@@ -1,11 +1,11 @@
 package android_serialport_api.base;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.ContentFrameLayout;
 import android.support.v7.widget.Toolbar;
@@ -18,7 +18,7 @@ import android.widget.Toast;
 /**
  * All Activities' parent Activity
  */
-public class BaseActivity extends AppCompatActivity {
+public class BaseActivity extends Activity {
     public Context mContext;
     protected Toolbar titleTB;
     protected AppCompatTextView toolbarTitle;
